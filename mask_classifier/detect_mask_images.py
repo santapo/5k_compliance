c932c1e9508359a8b787edcb383b6b7fd974e033
michaeltiat <michael@techainer.com>
2021-12-04 16:19:15 +0700

    change detect_mask_images only

--- mask_classifier/detect_mask_images.py
+++ mask_classifier/detect_mask_images.py
@@ -4,10 +4,12 @@
 # import the necessary packages
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
+import tflite_runtime.interpreter as tflite
 from tensorflow.keras.models import load_model
-from retinaface import RetinaFace
-from utils_5k import visualize_image
+# from retinaface import RetinaFace
+# from utils_5k import visualize_image
 import numpy as np
+import pandas as pd
 import argparse
 import logging
 import cv2
@@ -19,45 +21,129 @@ from tqdm import tqdm
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("detect_mask_images")
 
-def detect_single_mask(image_path, locs, mask_classifier):
-    image = cv2.imread(image_path)
+def detect_single_mask(image_path, faceNet):
+    locs = []
     faces = []
-    for loc in locs:
-        startX, startY, endX, endY = loc
-        startX = int(startX); startY = int(startY); endX = int(endX); endY = int(endY)
-        # import ipdb; ipdb.set_trace()
-        face = image[startY:endY, startX:endX]
-        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-        face = cv2.resize(face, (224, 224))
-        face = img_to_array(face)
-        face = preprocess_input(face)
-        faces.append(face)
+    # grab the dimensions of the frame and then construct a blob from it
+    image = cv2.imread(image_path)
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(image, 1.0, (640, 640),
+                                    (104.0, 177.0, 123.0))
 
-    # only make a prediction if at least one face was detected
-    if len(faces) > 0:
-        faces = np.array(faces, dtype="float32")
-        preds = mask_classifier.predict(faces, batch_size=32)
+    # pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
 
-    return (locs, preds)
+    # initialize our list of faces, their corresponding locations,
+    # and the list of predictions from our face mask network
+    
 
-def detect_mask_images(args, image_path_list, all_locs, visualize=False):
-    logger.info("Loading mask classification model...")
-    classifier = load_model(args.mask)
-    image_path_list = natsorted(image_path_list)
-    res_list = []
-    for image_path, locs in tqdm(zip(image_path_list, all_locs), total=len(image_path_list), desc="detecting mask"):
-        (_locs, preds) = detect_single_mask(image_path, locs, classifier)
-        if visualize:
-            image = cv2.imread(image_path)
-            visualize_image(image, _locs, preds)
-        res_list.append((_locs, preds, image_path))
-    return res_list
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            try:
+                # extract the face ROI
+                # startX, startY: top left corner
+                # endX, endY: bottom right corner
+                face = image[startY:endY, startX:endX]
+
+                # resize it to 160x160
+                face = cv2.resize(face, (160, 160))
+
+                # expand array shape from [160, 160, 3] to [1, 160, 160, 3]
+                face = np.expand_dims(face, axis=0)
+
+                # add the face and bounding boxes to their respective lists
+                faces.append(face)
+                locs.append((startY, startX, endY, endX))
+            except:
+                pass
+    return locs, faces
 
 
+def detect_mask_image(image_path, faceNet, interpreter, input_details, output_details):
+    locs, faces = detect_single_mask(image_path, faceNet)
+    '''tflite'''
+    labels = []
+    scores = []
+
+    for face in faces:
+        # pre-process image to conform to MobileNetV2
+        # input_mean = input_std = float(127.5)
+        # input_data = (np.float32(face) - input_mean) / input_std
+
+        # set our input tensor to our face image
+        interpreter.set_tensor(input_details[0]['index'], np.float32(face))
+
+        # perform classification
+        interpreter.invoke()
+
+        # get our output results tensor
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+        result = np.squeeze(output_data)
+
+        # get label from the result.
+        # the class with the higher confidence is the label.
+        (mask, withoutMask) = result
+        label = "Mask" if mask > withoutMask else "No Mask"
+
+        # get the highest confidence as the label's score
+        score = np.max(result)
+
+        labels.append(label)
+        scores.append(score)
+    return (labels, scores)
+
+def detect_all_images(image_path_list, face_path):
+    interpreter = tflite.Interpreter(
+        # model_path=os.path.expanduser("/home/pi/CollegeProject/models/MaskDetector/model.tflite"))
+        model_path=os.path.expanduser("model_quant.tflite"))
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    logger.info("Loading face detection model...")
+    prototxt_path = os.path.join(face_path, "deploy.prototxt")
+    weights_path = os.path.join(face_path, "res10_300x300_ssd_iter_140000.caffemodel")
+    faceNet = cv2.dnn.readNet(prototxt_path, weights_path)
+    
+    image_labels = []
+    for image_path in tqdm(image_path_list, desc='detecting mask'):
+        label, scores = detect_mask_image(image_path, faceNet, interpreter, input_details, output_details)
+        image_label = 0 if "No Mask" in label else 1
+        image_labels.append((image_path, image_label))
+    
+    return image_labels
 if __name__ == "__main__":
-    from parser_5k import set_parser
-    args = set_parser()
-    image_path_list = glob.glob(f'{args.images_dir}/*.jpg')
-    res = detect_mask_images(args, image_path_list)
-    print(res)
+    image_path_list = glob.glob('data/public_test/images/*.jpg')
+    all_res_mask = detect_all_images(image_path_list, 'face_model/')
+    with open('mask_detect_result.txt', 'w') as f:
+        f.write(str(all_res_mask))
+    df = pd.read_csv('distancing.csv')
+    res = []
+    for path, mask_label in all_res_mask:
+        path = os.path.basename(path)
+        final_label = int(df[df['fname'] == path]['distancing'])*mask_label
+        res.append((path, final_label))
+    from utils_5k import merge_result_to_meta
+    meta_path = "/home/michael/Michael/zaloai_challenge/5k_compliance/data/public_test/public_test_meta.csv"
+    final_df = merge_result_to_meta(meta_path, res)
+    final_df.to_csv("submit_public_test_person.csv")
 
