0ffb6a2ed317027250d553ebe27aad9315b5acb6
Santa Po <nguyentrongtuank62hust@gmail.com>
2021-12-05 12:29:34 +0700

    run with retinaface

--- 5k_compliance.py
+++ 5k_compliance.py
@@ -47,18 +47,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--images_dir", type=str, default=None,
                         help="Path to images directory")
-    parser.add_argument("-f", "--face", type=str, default="face_detector/model",
+    parser.add_argument("-f", "--face_weight", type=str, default="face_detector/model",
                         help="Path to face detection model directory")
-    parser.add_argument("-m", "--mask", type=str, default="mask_classifier/mask_detector.model",
+    parser.add_argument("-m", "--mask_weight", type=str, default="mask_classifier/mask_detector.model",
                         help="Path to mask classification model directory")
     parser.add_argument("-dc", "--detect_confidence", type=float, default=0.5,
                         help="Minimum probability to filer weak detections")
     args = vars(parser.parse_args())
 
     image_path_list = glob.glob(os.path.join(args["images_dir"], "*"))
-    detection_list = detect_mask_images(image_path_list, args["face"], args["mask"], args["detect_confidence"], visualize=False)
-
+    detection_list = detect_mask_images(image_path_list, args["face_weight"], args["mask_weight"], args["detect_confidence"], visualize=False)
+    import ipdb; ipdb.set_trace()
     result_list = []
+    distancing_compliance_df = pd.read_csv('distancing.csv')
     for locs, preds, image_path in detection_list:
         # locs: List of tuple, preds numpy array (N, 2)
         image_name = os.path.basename(image_path)
@@ -68,11 +69,12 @@ if __name__ == "__main__":
             result_list.append((image_name, is_5k_compliance))
             continue
         is_mask_compliance = mask_compliance(preds)
-        is_distancing_compliance = distancing_compliance(locs, dist_threshold=30)
+        is_distancing_compliance = int(distancing_compliance_df.loc[distancing_compliance_df['fname']==image_name]['distancing'])
+        #  is_distancing_compliance = distancing_compliance(locs, dist_threshold=30)
         is_5k_compliance = int(is_mask_compliance and is_distancing_compliance)
         result_list.append((image_name, is_5k_compliance))
-
-    meta_path = "/home/santapo/OnlineLab/5k_compliance_zalo/5k_compliance/data/public_test/public_test_meta.csv"
+    import ipdb; ipdb.set_trace()
+    meta_path = "/home/santapo/OnlineLab/challenges/5k_compliance_zalo/5k_compliance/data/public_test/public_test_meta.csv"
     final_df = merge_result_to_meta(meta_path, result_list)
     final_df.to_csv("submit_public_test.csv")    
 
